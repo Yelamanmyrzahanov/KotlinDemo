25b19957eb21895286e75d367b43e4e92d1f5928
Yelaman Myrzakhanov <yelamanmyrzahanov@gmail.com>
2020-03-17 00:37:10 +0600

    how the init method is used in Kotlin.

---
+++ src/InitDemo.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    class Person(var firstName: String, var secondName: String) {
+        var fullName: String?
+        init {
+            println("Init 1")
+            fullName = firstName+ " " + secondName
+        }
+
+        init {
+            println("Init 2")
+        }
+    }
+
+    val person = Person("Yela", "Myrza")
+    println(person.fullName)
+}
