a288f9ed144f98e9ff09c573b3ca40b64f3481c2
Yelaman Myrzakhanov <yelamanmyrzahanov@gmail.com>
2020-03-16 22:01:47 +0600

    Initial commit

---
+++ src/KotlinFunctions.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    var function: (Int, Int) -> Int = ::add
+
+    println(function(5, 3))
+
+    function = ::subtract
+
+    println(function(5, 3))
+
+    fun printRes(func: (Int, Int) -> Int, a: Int, b: Int) {
+        val result = func(a,b)
+        println(result)
+    }
+
+    printRes(::add,4,3)
+    printRes(::subtract,4,2)
+
+}
